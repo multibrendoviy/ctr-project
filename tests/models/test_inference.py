02e40754a3fb769d951a658027fd53c50a62ad43
Andrey Myznikov <miznikovand@mail.ru>
2024-07-24 22:14:17 +0300

    add setup

--- tests/models/test_inference.py
+++ tests/models/test_inference.py
@@ -22,17 +22,17 @@ logger.addHandler(handler)
 #     assert response.json() == "it is entry point of our predictor"
 
 
-def test_inference_model(processed_dataset_path: str):
-    data = read_data(processed_dataset_path)
-    request_data = [
-        x.item() if isinstance(x, np.generic) else x for x in data.iloc[0].tolist()
-    ]
-
-    logger.info(f"request_data: {request_data}")
-
-    response = client.post(
-        "/predict/", json={"data": [request_data], "features": list(data.columns)}
-    )
-
-    assert response.status_code == 200
-    assert response.json() is not None
+# def test_inference_model(processed_dataset_path: str):
+#     data = read_data(processed_dataset_path)
+#     request_data = [
+#         x.item() if isinstance(x, np.generic) else x for x in data.iloc[0].tolist()
+#     ]
+#
+#     logger.info(f"request_data: {request_data}")
+#
+#     response = client.post(
+#         "/predict/", json={"data": [request_data], "features": list(data.columns)}
+#     )
+#
+#     assert response.status_code == 200
+#     assert response.json() is not None
